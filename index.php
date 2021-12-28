b77e5a5d5e33b02832905b8451e227fbcee7121f
sahil78698 <79411549+sahil78698@users.noreply.github.com>
2021-12-28 13:37:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 $file_path = "uploads/";
 
 $file_path = $file_path . basename($_FILES['uploaded_file']['name']);
-   
+   if (!unlink($file_path + "HMM.zip")) { 
+    echo ("$file_pointer cannot be deleted due to an error"); 
+} 
+else { 
+    echo ("$file_pointer has been deleted"); 
+} 
 if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$file_path)){
     echo $_FILES['uploaded_file']['tmp_name'];
 }else{

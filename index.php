9b5660ce10279ebd2c987523d7ac0046ffe1178d
sahil78698 <79411549+sahil78698@users.noreply.github.com>
2021-12-28 14:41:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $files = glob('uploads/*'); // get all file names
 foreach($files as $file){ // iterate files
   if(is_file($file)) {
     unlink($file); // delete file
-         echo ("$file_path has been deleted"); 
+         echo ("$file has been deleted"); 
 
   }
 }

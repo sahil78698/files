836da752b71251673a1db0207fb685c7d93b99e7
sahil78698 <79411549+sahil78698@users.noreply.github.com>
2021-12-28 13:16:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $file_path = "uploads/";
 $file_path = $file_path . basename($_FILES['uploaded_file']['name']);
    
 if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$file_path)){
-    echo "success";
+    echo $_FILES['uploaded_file']['tmp_name'];
 }else{
     echo "fail";
 }

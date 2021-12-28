7f735b9b90d8a054c3bc6c1c59ab115aa498cac4
sahil78698 <79411549+sahil78698@users.noreply.github.com>
2021-12-28 12:17:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ $file_path = "uploads/";
 $file_path = $file_path . basename($_FILES['uploaded_file']['name']);
 if(!file_exists($file_path)){
 echo "okoko";
-}
+}else{
+ echo "lll";   
+}    
 if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$file_path)){
     echo "success";
 }else{
